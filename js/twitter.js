c8478e503d891cf4287a5612bb81bb0c3139586f
Maddie Rogers <madisonrogers@mymail.mines.edu>
2017-10-09 21:51:01 -0600

    added a function to get a random tweet

--- js/twitter.js
+++ js/twitter.js
@@ -2,20 +2,21 @@ var allTweets = [];
 
 function populateTweets() {
 	for(i = 0; i < data.length; i++) {
-		if(data[i].text.includes('Fake News') && !data[i].text.includes('http')) {
+		// Get all tweets containing Fake News and not containing http or ... (a continuation of a tweet)
+		if(data[i].text.includes('Fake News') && !data[i].text.includes('http') && !data[i].text.includes('...')) {
 			allTweets.push(data[i].text);
 		}
-		
-	}
-}
-
-function configure() {
-	populateTweets();
-	for(i=0;i<allTweets.length;i++) {
-		console.log(allTweets[i]);
 	}
 };
 
-configure();
+function getTweet() {
+	var randomNum = Math.floor(Math.random() * allTweets.length);
+	console.log(randomNum);
+	console.log(allTweets[randomNum]);
+	return allTweets[randomNum];
+};
 
+populateTweets();
+console.log(allTweets.length);
+getTweet();
 
